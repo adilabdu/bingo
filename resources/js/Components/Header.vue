e7b5a1c30d7954324825c0a45a948ccf712c35dd
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-03 12:57:05 +0300

    Add header component

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    value: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <label class="block font-medium text-lg ">
+        <span v-if="value">{{ value }}</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
