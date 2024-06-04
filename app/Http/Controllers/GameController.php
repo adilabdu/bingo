e11ca8017c23fbe00520e42bcd6bfcc4e5cfbed0
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-04 18:04:56 +0300

    Add load-cartela feature

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,22 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cartela;
 use App\Models\GameCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class GameController extends Controller
 {
     public function index()
     {
         return Inertia::render('Game/Initiate/Index', [
-            'categories' => GameCategory::all()
+            'gameCategories' => GameCategory::all()
         ]);
     }
 
-    public function selectCartela()
+    public function selectCartela($categoryId, $cartelaName): Response
     {
-        return Inertia::render('Game/Initiate/Cartela');
+        return Inertia::render('Game/Initiate/Cartela',[
+            'gameCategory' => GameCategory::findOrFail($categoryId),
+            'cartela' => Inertia::lazy(function () use ($cartelaName) {
+                $l = Cartela::where('name', $cartelaName)->first();
+                Log::info($l);
+                return $l;
+            })
+        ]);
     }
 
     public function playGame()
--- resources/js/Pages/Game/Initiate/Cartela.vue
+++ resources/js/Pages/Game/Initiate/Cartela.vue
@@ -3,9 +3,12 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Header from "@/Components/Header.vue";
 import {Input} from "@/Components/shadcn/ui/input/index.js";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import ConfirmCartelaDrawer from "@/Views/Game/ConfirmCartelaDrawer.vue";
+import {router, usePage} from "@inertiajs/vue3";
+import {debounce} from "lodash";
+import Loading from "@/Components/Loading.vue";
 
 const numberOfCards = ref(1);
 
@@ -16,49 +19,74 @@ const updateNumberOfCards = (action) => {
         numberOfCards.value--;
     }
 }
+
+const gameCategory = usePage().props.gameCategory;
+const cartelaName = ref('');
+const isLoading = ref(false);
+
+watch(cartelaName, () => {
+    getCartela();
+}, {});
+
+const getCartela = debounce(() => {
+    isLoading.value = true;
+    router.visit(cartelaName.value,{
+        preserveState: true,
+        replace: true,
+        only: ['cartela'],
+        onFinish: () => {
+            isLoading.value = false;
+        }
+    });
+}, 500);
 </script>
 
 <template>
-<AuthenticatedLayout>
-    <Header>
-        <template #default>
-            <div class="font-semibold text-lg pb-1">Customize your Cartela</div>
-            <div class="text-xs font-light">Cartela is a set of playing cards. You can play with up to 3 cards!</div>
-        </template>
-    </Header>
+    <AuthenticatedLayout>
+        <div class="flex flex-col space-y-2 bg-gray-800 text-white rounded-lg p-3 capitalize">
+            <div class="font-bold text-3xl"> {{gameCategory.amount}} Br</div>
+            <div class="font-light text-sm">{{gameCategory.name}} X {{gameCategory.category}}</div>
+        </div>
+        <Header>
+            <template #default>
+                <div class="font-semibold text-xl pb-1">Customize your Cartela</div>
+                <div class="text-xs font-light">Cartela is a set of playing cards. You can play with up to 3 cards!</div>
+            </template>
+        </Header>
 
-    <div class="flex flex-col space-y-2 py-3">
-        <InputLabel value="Please select number of cartela's"/>
-        <div class="flex justify-between">
-            <div  @click="updateNumberOfCards('decrement')"
-                  class="bg-gray-800  text-white font-semibold font w-12 text-center flex items-center justify-center rounded-md">
-                -
-            </div>
-            <Input type="text" class=" w-8/12" v-model="numberOfCards"/>
-            <div
-                @click="updateNumberOfCards('increment')"
-                class="bg-gray-800 text-white font-semibold font w-12 text-center flex items-center justify-center rounded-md">
-                +
+        <div class="flex flex-col space-y-2 py-3">
+            <InputLabel value="Please select number of cartela's"/>
+            <div class="flex justify-between">
+                <div  @click="updateNumberOfCards('decrement')"
+                      class="bg-gray-800  text-white font-semibold font w-12 text-center flex items-center justify-center rounded-md">
+                    -
+                </div>
+                <Input type="text" class=" w-8/12" v-model="numberOfCards"/>
+                <div
+                    @click="updateNumberOfCards('increment')"
+                    class="bg-gray-800 text-white font-semibold font w-12 text-center flex items-center justify-center rounded-md">
+                    +
+                </div>
             </div>
         </div>
-    </div>
 
 
-    <div class="flex flex-col space-y-2 py-3">
-        <InputLabel value="Enter your lucky cartela number"/>
-        <div class="flex">
-            <div  @click="updateNumberOfCards('decrement')"
-                  class="bg-gray-800 text-white font-semibold  w-12 text-center flex items-center justify-center rounded-l-md">
-                #
+        <div class="flex flex-col space-y-2 py-3">
+            <InputLabel value="Enter your lucky cartela number"/>
+            <div class="flex">
+                <div  @click="updateNumberOfCards('decrement')"
+                      class="bg-blue-600 text-white font-semibold w-14 text-center flex items-center justify-center rounded-l-md">
+                    <Loading v-if="isLoading" type="spinner" class="p-2" color="primary"/>
+                    <span v-else>#</span>
+                </div>
+                <Input type="number" class=" !rounded-none !rounded-r-md w-full" v-model="cartelaName"/>
             </div>
-            <Input type="number" class=" !rounded-none !rounded-r-md w-full"/>
-        </div>
             <div class="text-xs font-light">You can select from # 1 - 3000</div>
-    </div>
+        </div>
 
 
-    <ConfirmCartelaDrawer />
-</AuthenticatedLayout>
+        <ConfirmCartelaDrawer />
+    </AuthenticatedLayout>
 </template>
 
 <style scoped>
