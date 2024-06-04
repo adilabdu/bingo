b83927de51cbc481a46dc1b57ba6bf9fe7321ef6
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-04 11:54:12 +0300

    Add 'app:populate-game-category' command - Populates game category, should be run once

---
+++ app/Console/Commands/PopulateGameCategories.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\GameCategory;
+use Illuminate\Console\Command;
+
+class PopulateGameCategories extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:populate-game-categories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Populate the game categories table with some default data.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Add some default game categories
+        $gameCategories = [
+            ['name' => 'Bronze', 'amount' => 10, 'category' => 'basic'],
+            ['name' => 'Silver', 'amount' => 15, 'category' => 'basic'],
+            ['name' => 'Gold', 'amount' => 20, 'category' => 'basic'],
+            ['name' => 'Platinum', 'amount' => 30, 'category' => 'basic'],
+            ['name' => 'Diamond', 'amount' => 50, 'category' => 'basic'],
+            ['name' => 'Master', 'amount' => 100, 'category' => 'basic'],
+            ['name' => 'Grandmaster', 'amount' => 200, 'category' => 'vip'],
+            ['name' => 'Challenger', 'amount' => 500, 'category' => 'vip'],
+            ['name' => 'Legend', 'amount' => 1000, 'category' => 'vip'],
+            ['name' => 'Immortal', 'amount' => 2000, 'category' => 'vip'],
+        ];
+
+        foreach ($gameCategories as $gameCategory) {
+            GameCategory::create($gameCategory);
+        }
+
+        $this->info('Game categories table populated successfully.');
+    }
+}
