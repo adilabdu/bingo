0b069caad446ca41fe688d2620e6d0f7aad160ea
adilabdubushra <adilabdu68@gmail.com>
2024-06-14 20:54:43 +0300

    Implement 'Wallet' Page with Tranfer feature - New `/wallet` page for players - New wallet page shows player's current balance - New form to transfer money to another player
    #### Updates
    - Add additional seeded Player inside `DatabaseSeeder`
    - Add link to new wallet page in `AuthenticatedLayout`
    - Add `phone_number` attribute to `users` table

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatToPrice(value) {
+    return Number(value.toFixed(2)).toLocaleString("en-US");
+}
