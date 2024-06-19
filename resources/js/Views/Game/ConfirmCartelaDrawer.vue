db500ac489d75831a5c13d69367290a7957e0d65
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-19 19:36:37 +0300

    Fix scroll bug

--- resources/js/Views/Game/ConfirmCartelaDrawer.vue
+++ resources/js/Views/Game/ConfirmCartelaDrawer.vue
@@ -58,37 +58,40 @@ function startBingo() {
         <DrawerPortal>
             <DrawerOverlay class="z-50 fixed bg-black/40 inset-0" />
             <DrawerContent
-                class="bg-gray-100 flex flex-col space-y-4 rounded-t-xl !h-full max-h-[calc(100%-(env(safe-area-inset-top)+51px+1rem))] fixed bottom-0 left-0 right-0 z-[60] px-5"
+                class="bg-gray-100 flex flex-col rounded-t-xl !h-full max-h-[calc(100%-(env(safe-area-inset-top)+51px+1rem))] fixed bottom-0 left-0 right-0 z-[60]"
             >
-                <div
-                    class="mt-4 mx-auto w-14 h-1.5 flex-shrink-0 rounded-full bg-gray-300"
-                />
+                <div class="overflow-y-auto flex flex-col space-y-5 w-full px-5 py-6">
 
-                <div class="text-center font-semibold text-2xl">
-                    Confirm Your Cartela
-                </div>
+                    <div
+                        class=" mx-auto w-14 h-1.5 flex-shrink-0 rounded-full bg-gray-300"
+                    />
 
-                <div>
-                    <div class="py-3 rounded-lg flex justify-between items-center divide-white divide-x text-white bg-gradient-to-br from-blue-600 to-sky-600">
+                    <div class="text-center font-semibold text-2xl">
+                        Confirm Your Cartela
+                    </div>
+
+                    <div>
+                        <div class="py-3 rounded-lg flex justify-between items-center divide-white divide-x text-white bg-gradient-to-br from-blue-600 to-sky-600">
                         <span class="w-6/12 text-center flex flex-col items-center space-y-2">
                             <span class="font-bold text-3xl">#{{ cartela?.name }}</span>
                             <span class="text-sm">
                                 Cartela No
                             </span>
                         </span>
-                        <span class="w-6/12 text-center flex flex-col items-center">
+                            <span class="w-6/12 text-center flex flex-col items-center">
                             <span class="font-bold text-3xl">
                             {{gameCategory.amount}} <span class="font-light text-xl">Br</span>
                             </span>
                             <span class="font-light text-sm">Category</span>
                         </span>
+                        </div>
+                        <BingoBoard v-if="cartelaNumbers" :numbers="cartelaNumbers" card-size="w-14" />
                     </div>
-                    <BingoBoard v-if="cartelaNumbers" :numbers="cartelaNumbers" card-size="w-14" />
-                </div>
 
-                <Button @click="startBingo" class="bg-blue-600 text-white font-semibold w-full">
-                    Start Bingo
-                </Button>
+                    <Button @click="startBingo" class="bg-blue-600 text-white font-semibold w-full">
+                        Start Bingo
+                    </Button>
+                </div>
             </DrawerContent>
         </DrawerPortal>
     </DrawerRoot>
