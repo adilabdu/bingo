7d2f97bfa85954d897f3a89a32bd8a61646efac8
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-19 19:51:16 +0300

    Add parent layouts on app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,42 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
+import {createApp, h, provide, ref} from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+const notificationData = ref(null);
+
+const showNotification = (data) => {
+    notificationData.value = data;
+};
+
+// TODO: Migrate the two providers
+provide("showNotification", showNotification);
+provide("notificationData", notificationData);
+
+function getLayout(name) {
+    switch (true) {
+        case name.startsWith("Admin/"):
+            return AdminLayout;
+
+        default:
+            return AuthenticatedLayout;
+    }
+}
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        let page = pages[`./Pages/${name}.vue`];
+        page.default.layout = getLayout(name, page);
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
