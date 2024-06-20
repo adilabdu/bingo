cc1eb042c1823f7550f8433847b3436e50f0c0eb
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-20 12:05:47 +0300

    Update your register UI

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,143 +1,111 @@
-<script setup lang="ts">
-import AdminLayout from '@/Layouts/AdminLayout.vue';
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from '@/Components/shadcn/ui/select/index.js';
-
 const form = useForm({
     name: '',
     email: '',
-    phone_number: '',
     password: '',
     password_confirmation: '',
-    type: ''
 });
 
 const submit = () => {
     form.post(route('register'), {
-        onFinish: () => form.reset('password', 'password_confirmation')
+        onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
 </script>
 
 <template>
-    <AdminLayout>
-        <Head title="Register" />
 
-        <form @submit.prevent="submit" class="w-10/12 flex flex-col space-y-5 mx-auto p-6 bg-white shadow-lg rounded-lg">
-            <h1 class="text-2xl font-bold mb-6 text-center">
-                Register New User
-            </h1>
+    <div class="px-4 rounded-lg">
+    <Head title="Register" />
 
-            <div>
-                <InputLabel for="type" value="User Type" />
-                <Select v-model="form.type">
-                    <SelectTrigger class="w-full">
-                        <SelectValue placeholder="Select user type" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>User Type</SelectLabel>
-                            <SelectItem value="admin">Admin</SelectItem>
-                            <SelectItem value="player">Player</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-                <InputError class="mt-2" :message="form.errors.type" />
-            </div>
+        <div class="text-4xl pb-10">
+            Sign up and start winning!
+        </div>
+    <form @submit.prevent="submit">
+        <div>
+            <InputLabel for="name" value="Name" />
 
-            <div v-if="form.type">
-                <InputLabel for="name" value="Name" />
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full border"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-                <InputError class="mt-2" :message="form.errors.name" />
-            </div>
+            <TextInput
+                id="name"
+                type="text"
+                class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                v-model="form.name"
+                required
+                autofocus
+                autocomplete="name"
+                placeholder="Abebe"
+            />
 
-            <div v-if="form.type === 'admin'" class="mt-4">
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full border"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+            <InputError class="mt-2" :message="form.errors.name" />
+        </div>
 
-            <div v-if="form.type === 'player'" class="mt-4">
-                <InputLabel for="phone" value="Phone Number" />
-                <TextInput
-                    id="phone"
-                    type="tel"
-                    class="mt-1 block w-full border"
-                    v-model="form.phone_number"
-                    required
-                    autocomplete="tel"
-                />
-                <InputError class="mt-2" :message="form.errors.phone_number" />
-            </div>
+        <div class="mt-4">
+            <InputLabel for="email" value="Email" />
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full border"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+            <TextInput
+                id="email"
+                type="email"
+                class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                v-model="form.email"
+                required
+                autocomplete="username"
+                placeholder="Kebede"
+            />
 
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full border"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
+            <InputError class="mt-2" :message="form.errors.email" />
+        </div>
 
-            <div class="flex items-center justify-end mt-6">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Already registered?
-                </Link>
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
-    </AdminLayout>
+        <div class="mt-4">
+            <InputLabel for="password" value="Password" />
+
+            <TextInput
+                id="password"
+                type="password"
+                class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                v-model="form.password"
+                required
+                autocomplete="new-password"
+                placeholder="New Password "
+            />
+
+            <InputError class="mt-2" :message="form.errors.password" />
+        </div>
+
+        <div class="mt-4">
+            <InputLabel for="password_confirmation" value="Confirm Password" />
+
+            <TextInput
+                id="password_confirmation"
+                type="password"
+                class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                v-model="form.password_confirmation"
+                required
+                autocomplete="new-password"
+                placeholder="Confirm Password"
+            />
+
+            <InputError class="mt-2" :message="form.errors.password_confirmation" />
+        </div>
+
+        <div class="flex items-center justify-between mt-6">
+            <Link
+                :href="route('login')"
+                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Already registered?
+            </Link>
+
+            <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                Register
+            </PrimaryButton>
+        </div>
+    </form>
+    </div>
 </template>
-
-<style scoped>
-</style>
