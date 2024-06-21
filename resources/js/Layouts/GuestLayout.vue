d081f302dd2292931267133b31faa2f7f9a2fbd8
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-21 20:09:22 +0300

    Update footer brand

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
         <div
-            class="w-full sm:max-w-md mt-6 px-2 py-4 overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-md lg:max-w-5xl px-2 py-4 overflow-hidden sm:rounded-lg"
         >
             <slot />
         </div>
@@ -12,8 +12,8 @@
         <footer class="absolute bottom-1 mt-6">
             <div class="container mx-auto py-4 px-4 sm:px-6 lg:px-8">
                 <p class="text-center text-sm text-gray-500">© 2024
-                    <span class="font-semibold text-black">
-                    <span class="text-[#8ee53f]">Kiwi</span> Bingo</span>. All rights reserved.</p>
+                    <span class="font-semibold text-brand-secondary">
+                    <span class="text-brand-primary">Kiwi</span> Bingo</span>. All rights reserved.</p>
             </div>
         </footer>
     </div>
