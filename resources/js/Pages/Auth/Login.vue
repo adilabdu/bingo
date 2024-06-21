4c8176e1f153e077cba0de2cce3ecf0b4ffcf3bd
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-21 20:03:25 +0300

    Update login and register pages UI to be be desktop responsive

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,88 +31,103 @@ const submit = () => {
 </script>
 
 <template>
-    <div class="px-3">
-        <div class="w-full flex justify-between items-center pb-10">
+    <div class="px-3 w-full flex justify-between">
+        <div class="w-full lg:w-7/12 max-w-sm">
+            <div class="flex justify-between items-center pb-10">
+                <div class="text-4xl lg:text-5xl w-full">
+                    Sign in to your account
+                </div>
 
-            <div class="text-4xl w-full">
-                Sign in to your account
+                <div class="flex w-3/12 justify-end lg:hidden">
+                    <img class="w-full object-cover" src="../../../../public/assets/images/logo.png">
+                </div>
             </div>
+            <form @submit.prevent="submit">
+                <div class="flex flex-col space-y-2">
+                    <InputLabel for="login" value="Phone Number" />
+                    <div class="flex items-center font-semibold min-h-full">
+                        <div class="bg-brand-primary text-white px-3 py-3.5 h-full  rounded-l-lg min-h-full ">+251</div>
 
-            <div class="flex w-3/12 justify-end">
-            <img class="w-full object-cover" src="../../../../public/assets/images/logo.png">
+                        <TextInput
+                            id="login"
+                            type="number"
+                            class="block w-full !bg-gray-50 !border-black !border !rounded-none !rounded-r-lg"
+                            v-model="form.login"
+                            required
+                            autofocus
+                            autocomplete="number"
+                            placeholder="(09/07)12345678"
+                        />
+
+                    </div>
+                    <InputError class="mt-2" :message="form.errors.login" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="password" value="Password" />
+
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full !bg-gray-50 !border-black !border"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                        placeholder="Password"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="flex justify-between items-center py-2 mt-4">
+                    <label class="flex items-center">
+                        <Checkbox name="remember" v-model:checked="form.remember" />
+                        <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    </label>
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Forgot your password?
+                    </Link>
+                </div>
+
+                <div class="flex items-center text-center mt-4">
+
+
+                    <PrimaryButton class="w-full flex justify-center !bg-brand-150" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Log in
+                    </PrimaryButton>
+                </div>
+
+                <div class="flex justify-center py-3">
+                    <Link
+                        :href="route('register')"
+                        class="text-center text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                    >
+                        Don't have an account? <span class="underline">
+                    Register
+                </span>
+                    </Link>
+                </div>
+            </form>
+
+        </div>
+        <div class="hidden lg:flex flex-col space-y-2 w-4/12 text-center rounded-lg justify-center items-center">
+
+
+            <div class="flex w-full justify-center">
+                <img class="w-7/12 object-cover" src="../../../../public/assets/images/logo.png">
+            </div>
+            <div class="text-4xl font-bold text-brand-100">
+                <span class="text-brand-150">Kiwi</span>
+                Bingo
+            </div>
+            <div class="text-sm">
+                Embrace the fun of playing bingo online!
             </div>
 
         </div>
-        <form @submit.prevent="submit">
-            <div class="flex flex-col space-y-2">
-                <InputLabel for="login" value="Phone Number" />
-                <div class="flex items-center font-semibold min-h-full">
-                    <div class="bg-brand-150 text-white px-3 py-3.5 h-full  rounded-l-lg min-h-full ">+251</div>
-
-                    <TextInput
-                        id="login"
-                        type="number"
-                        class="block w-full !bg-gray-50 !border-black !border !rounded-none !rounded-r-lg"
-                        v-model="form.login"
-                        required
-                        autofocus
-                        autocomplete="number"
-                        placeholder="(09/07)12345678"
-                    />
-
-                </div>
-                    <InputError class="mt-2" :message="form.errors.login" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full !bg-gray-50 !border-black !border"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                    placeholder="Password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="flex justify-between items-center py-2 mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-            </div>
-
-            <div class="flex items-center text-center mt-4">
-
-
-                <PrimaryButton class="w-full flex justify-center !bg-brand-150" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-
-            <div class="flex justify-center py-3">
-                <Link
-                    :href="route('register')"
-                    class="text-center text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                >
-                    Don't have an account? <span class="underline">
-                    Register
-                </span>
-                </Link>
-            </div>
-        </form>
-
     </div>
 </template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -24,95 +24,111 @@ const submit = () => {
 
 <template>
 
-    <div class="px-4 rounded-lg">
-        <Head title="Register" />
-
-        <div class="text-4xl pb-10">
-            Sign up and start winning!
-        </div>
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Full Name" />
-
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                    placeholder="Abebe Kebede"
-                />
-
-                <InputError class="mt-2" :message="form.errors.name" />
+    <div class="px-4 w-full flex justify-between">
+        <div class="w-full lg:w-7/12 max-w-sm">
+            <div class="text-4xl pb-10">
+                Sign up and start winning!
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="phone_number" value="Phone Number" />
-
-                <div class="flex items-center font-semibold min-h-full">
-                    <div class="bg-black text-white px-3 py-3.5 h-full  rounded-l-lg min-h-full ">+251</div>
+            <form @submit.prevent="submit">
+                <div>
+                    <InputLabel for="name" value="Full Name" />
 
                     <TextInput
-                        id="phone_number"
-                        type="number"
-                        class=" block w-full !bg-gray-50 !border-gray-900 !border !rounded-none !rounded-r-lg"
-                        v-model="form.phone_number"
+                        id="name"
+                        type="text"
+                        class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                        v-model="form.name"
                         required
-                        autocomplete="number"
-                        placeholder="(9/7)11223344"
+                        autofocus
+                        autocomplete="name"
+                        placeholder="Abebe Kebede"
                     />
+
+                    <InputError class="mt-2" :message="form.errors.name" />
                 </div>
 
-                <InputError class="mt-2" :message="form.errors.phone_number" />
+                <div class="mt-4">
+                    <InputLabel for="phone_number" value="Phone Number" />
+
+                    <div class="flex items-center font-semibold min-h-full">
+                        <div class="bg-brand-primary text-white px-3 py-3.5 h-full  rounded-l-lg min-h-full ">+251</div>
+
+                        <TextInput
+                            id="phone_number"
+                            type="number"
+                            class=" block w-full !bg-gray-50 !border-gray-900 !border !rounded-none !rounded-r-lg"
+                            v-model="form.phone_number"
+                            required
+                            autocomplete="number"
+                            placeholder="(9/7)11223344"
+                        />
+                    </div>
+
+                    <InputError class="mt-2" :message="form.errors.phone_number" />
+                </div>
+
+
+                <div class="mt-4">
+                    <InputLabel for="password" value="Password" />
+
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                        v-model="form.password"
+                        required
+                        autocomplete="new-password"
+                        placeholder="New Password "
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                    <TextInput
+                        id="password_confirmation"
+                        type="password"
+                        class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
+                        v-model="form.password_confirmation"
+                        required
+                        autocomplete="new-password"
+                        placeholder="Confirm Password"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                </div>
+
+                <div class="flex items-center justify-between mt-6">
+                    <Link
+                        :href="route('login')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Already registered?
+                    </Link>
+
+                    <PrimaryButton class="ms-4 px-6" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Register
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
+        <div class="hidden lg:flex flex-col space-y-2 w-4/12 text-center rounded-lg justify-center items-center">
+
+
+            <div class="flex w-full justify-center">
+                <img class="w-7/12 object-cover" src="../../../../public/assets/images/logo.png">
+            </div>
+            <div class="text-4xl font-bold text-brand-100">
+                <span class="text-brand-150">Kiwi</span>
+                Bingo
+            </div>
+            <div class="text-sm">
+                Embrace the fun of playing bingo online!
             </div>
 
+        </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                    placeholder="New Password "
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full !bg-gray-50 !border-gray-900 !border"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                    placeholder="Confirm Password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-between mt-6">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton class="ms-4 px-6" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
     </div>
 </template>
