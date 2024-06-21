35183e29ef58ff7dfddf9744e9b9b30a122ac83f
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-22 01:00:22 +0300

    Update wallet page to be desktop responsive

--- resources/js/Views/Wallet/WalletPageHeader.vue
+++ resources/js/Views/Wallet/WalletPageHeader.vue
@@ -5,8 +5,8 @@ import Header from "@/Components/Header.vue";
 <template>
     <Header class="max-w-md">
         <template #default>
-            <div class="font-semibold text-xl pb-1">Your Wallet</div>
-            <div class="text-xs font-light">You can withdraw from or deposit to your wallet account. You can also transfer funds to another player.</div>
+            <div class="font-semibold text-xl md:text-4xl pb-1">Your Wallet</div>
+            <div class="text-xs md:text-sm font-light">You can withdraw from or deposit to your wallet account. You can also transfer funds to another player.</div>
         </template>
     </Header>
 </template>
