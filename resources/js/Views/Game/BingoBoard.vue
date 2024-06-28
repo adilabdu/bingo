46d613d341d627c48115edf8692d2b5fb5b8b486
adilabdubushra <adilabdu68@gmail.com>
2024-06-28 19:10:35 +0300

    Fix issue where unrevealed numbers can be clicked

--- resources/js/Views/Game/BingoBoard.vue
+++ resources/js/Views/Game/BingoBoard.vue
@@ -36,13 +36,17 @@ const props = defineProps({
 
 const emits = defineEmits(['bingo', 'finish']);
 
-const { addToClickedNumbers, clickedNumbers } = useGameDataStore();
+const gameStore = useGameDataStore();
 
 const handleClick = (number) => {
     if (winnerNumbers.includes(number)) return;
-    if (props.drawnNumbers.includes(number) && !clickedNumbers.has(number)) {
-        clickedNumbers.add(number);
-        addToClickedNumbers(number);
+    if (
+        props.drawnNumbers.includes(number) &&
+        props.drawnNumbers.indexOf(number) <= gameStore.revealIndex &&
+        !gameStore.clickedNumbers.has(number)
+    ) {
+        gameStore.clickedNumbers.add(number);
+        gameStore.addToClickedNumbers(number);
     }
 };
 
@@ -58,10 +62,10 @@ const formattedBingoData = computed(() => {
     });
 });
 
-watch(() => Array.from(clickedNumbers), () => {
-    if (Array.from(clickedNumbers).length > 3) {
-        if (useIsBoardWinner(Array.from(clickedNumbers), formattedBingoData.value)) {
-            emits('bingo', Array.from(clickedNumbers));
+watch(() => Array.from(gameStore.clickedNumbers), () => {
+    if (Array.from(gameStore.clickedNumbers).length > 3) {
+        if (useIsBoardWinner(Array.from(gameStore.clickedNumbers), formattedBingoData.value)) {
+            emits('bingo', Array.from(gameStore.clickedNumbers));
         }
     }
 });
@@ -85,9 +89,9 @@ Echo.private('game-result')
 
 
 const getClassForNumber = (num) => {
-    if (num === props.currentDrawnNumber && !clickedNumbers.has(num)) {
+    if (num === props.currentDrawnNumber && !gameStore.clickedNumbers.has(num)) {
         return 'animate-pulse bg-brand-tertiary text-white';
-    } else if (clickedNumbers.has(num)) {
+    } else if (gameStore.clickedNumbers.has(num)) {
         return 'bg-brand-secondary text-white';
     } else if (winnerNumbers.includes(num)) {
         return 'bg-black text-white';
