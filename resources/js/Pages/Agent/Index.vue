712877e1e9b44b04e11a204ca2f98f8bc549e624
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-19 12:54:55 +0300

    Update agent UI

--- resources/js/Pages/Agent/Index.vue
+++ resources/js/Pages/Agent/Index.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { usePage } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import {usePage} from '@inertiajs/vue3';
+import {ref} from 'vue';
 import Modal from '@/Components/Modal.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Table, TableBody, TableRow, TableCell } from '@/Components/shadcn/ui/table/index.js';
-import { useForm } from '@inertiajs/vue3';
+import {Table, TableBody, TableRow, TableCell} from '@/Components/shadcn/ui/table/index.js';
+import {useForm} from '@inertiajs/vue3';
+import OverViewItem from "@/Views/Agent/Dashboard/OverViewItem.vue";
+import Header from "@/Components/Header.vue";
+import moment from "moment";
 
 const page = usePage();
 const agent = page.props.auth.user;
@@ -38,66 +41,39 @@ const submit = () => {
 </script>
 
 <template>
-    <div class="bg-white rounded-lg p-6">
-        <div class="mb-6">
-            <h2 class="text-3xl font-bold mb-2">Welcome, {{ agent.name }}</h2>
-            <p class="text-gray-600">Manage your branches and cashiers below.</p>
+    <div class="flex flex-col space-y-4">
+        <div class="mb-6 hidden md:inline-block">
+            <h2 class="text-2xl md:text-3xl font-semibold mb-2">Welcome, {{ agent.name }}</h2>
+            <p class="text-gray-600 hidden md:inline-flex">Manage your branches and cashiers below.</p>
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
-            <div class="bg-blue-100 p-4 rounded-lg ">
-                <h3 class="text-xl font-semibold mb-2">Total Branches</h3>
-                <p v-if="branches?.length > 0" class="text-lg">{{ branches?.length }}</p>
-                <p v-else class="text-lg">0</p>
-            </div>
-            <div class="bg-green-100 p-4 rounded-lg ">
-                <h3 class="text-xl font-semibold mb-2">Total Cashiers</h3>
-                <p class="text-lg">
-                    {{ branches.reduce((acc, branch) => acc + branch.cashiers.length, 0) }}
-                </p>
-            </div>
-            <div class="bg-blue-100 p-4 rounded-lg ">
-                <h3 class="text-xl font-semibold mb-2">Total Revenue</h3>
-                <p class="text-lg">
-                    {{ totalRevenue }}
-                </p>
+        <div>
+            <Header class="font-semibold" value="Revenue Numbers"/>
+            <div class="flex flex-wrap justify-between">
+                <OverViewItem base-class="bg-lime-100" label="Today" value="1650 Br"/>
+                <OverViewItem base-class="bg-emerald-100" label="This Week" value="16950 Br"/>
+                <OverViewItem base-class="bg-purple-100" label="This Month" value="50000 Br"/>
+                <OverViewItem base-class="bg-zinc-200" label="Total" :value="totalRevenue + ' Br'"/>
             </div>
         </div>
 
         <div class="mb-6 flex justify-end">
-            <PrimaryButton @click="showCreateBranchModal">
+            <PrimaryButton class="!bg-brand-secondary" @click="showCreateBranchModal">
                 Add Branch
             </PrimaryButton>
         </div>
-
-        <!-- Top Performing Branches -->
-        <div class="mb-6">
-            <h3 class="text-2xl font-bold mb-4">Top Performing Branches</h3>
-            <Table class="min-w-full bg-white shadow rounded-lg">
-                <TableBody class="bg-gray-100 font-semibold">
-                    <TableRow>
-                        <TableCell class="p-4">Branch Name</TableCell>
-                        <TableCell class="p-4">Location</TableCell>
-                        <TableCell class="p-4">Total Transactions</TableCell>
-                    </TableRow>
-                </TableBody>
-                <TableBody>
-                    <TableRow v-for="branch in topBranches" :key="branch.id" class="border-b">
-                        <TableCell class="p-4">{{ branch.name }}</TableCell>
-                        <TableCell class="p-4">{{ branch.location }}</TableCell>
-                        <TableCell class="p-4">{{ branch.transactions.length }}</TableCell>
-                    </TableRow>
-                    <TableRow v-if="topBranches.length === 0">
-                        <TableCell class="p-4 text-center" colspan="3">No top-performing branches found.</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+        <div>
+            <Header class="font-semibold" value="Your Stats"/>
+            <div class="flex flex-wrap justify-between">
+                <OverViewItem base-class="bg-blue-100" label="Total Cashiers" :value="branches.reduce((acc, branch) => acc + branch.cashiers.length, 0)"/>
+                <OverViewItem base-class="bg-pink-100" label="Total Branches" :value="branches?.length ?? 0"/>
+            </div>
         </div>
 
 
         <!-- Recent Activities -->
-        <div class="mb-6">
-            <h3 class="text-2xl font-bold mb-4">Recent Activities</h3>
+        <div class="flex flex-col space-y-4">
+            <Header class="font-semibold" value="Recent Activities"/>
             <div class="overflow-x-auto">
                 <Table class="min-w-full bg-white shadow rounded-lg">
                     <TableBody class="bg-gray-100 font-semibold">
@@ -108,8 +84,8 @@ const submit = () => {
                     </TableBody>
                     <TableBody>
                         <TableRow v-for="activity in recentActivities" :key="activity.id" class="border-b">
-                            <TableCell class="p-4">{{ activity.description }}</TableCell>
-                            <TableCell class="p-4">{{ new Date(activity.created_at).toLocaleString() }}</TableCell>
+                            <TableCell class="p-4 !text-xs md:!text-sm capitalize">{{ activity.description }}</TableCell>
+                            <TableCell class="p-4 !text-xs md:text-sm capitalize">{{ moment(activity.created_at).fromNow() }}</TableCell>
                         </TableRow>
                         <TableRow v-if="recentActivities.length === 0">
                             <TableCell class="p-4 text-center" colspan="2">No recent activities available.</TableCell>
@@ -127,7 +103,7 @@ const submit = () => {
                     <h3 class="text-xl font-semibold mb-4">Create Branch</h3>
                     <form @submit.prevent="submit">
                         <div class="mb-4">
-                            <InputLabel for="name" value="Branch Name" />
+                            <InputLabel for="name" value="Branch Name"/>
                             <TextInput
                                 id="name"
                                 type="text"
@@ -136,11 +112,11 @@ const submit = () => {
                                 required
                                 autofocus
                             />
-                            <InputError :message="form.errors.name" class="mt-2" />
+                            <InputError :message="form.errors.name" class="mt-2"/>
                         </div>
 
                         <div class="mb-4">
-                            <InputLabel for="location" value="Location" />
+                            <InputLabel for="location" value="Location"/>
                             <TextInput
                                 id="location"
                                 type="text"
@@ -148,12 +124,15 @@ const submit = () => {
                                 v-model="form.location"
                                 required
                             />
-                            <InputError :message="form.errors.location" class="mt-2" />
+                            <InputError :message="form.errors.location" class="mt-2"/>
                         </div>
 
                         <div class="flex justify-end mt-6">
                             <PrimaryButton :disabled="form.processing">Create</PrimaryButton>
-                            <button @click="isCreateBranchModalOpen = false" type="button" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded-lg shadow hover:bg-gray-600 transition">Cancel</button>
+                            <button @click="isCreateBranchModalOpen = false" type="button"
+                                    class="ml-2 bg-gray-500 text-white px-4 py-2 rounded-lg shadow hover:bg-gray-600 transition">
+                                Cancel
+                            </button>
                         </div>
                     </form>
                 </div>
---
+++ resources/js/Views/Agent/Dashboard/OverViewItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+    baseClass: {
+        type: String,
+        default:'bg-gray-100 text-black'
+    },
+    label: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div
+        :class="baseClass"
+        class="flex flex-col space-y-1 my-2 w-5/12 p-2 rounded-md items-center max-w-sm">
+        <span class="text-[0.70rem] font-light">{{ label }}</span>
+        <span class="font-semibold text-lg">{{ value }}</span>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
