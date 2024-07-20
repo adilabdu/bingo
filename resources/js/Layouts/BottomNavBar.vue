88c9a3d48b054fa4cf690608f8302cbc08d3b055
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-20 15:03:26 +0300

    Add a bottom navbar menu for cashiers

--- resources/js/Layouts/BottomNavBar.vue
+++ resources/js/Layouts/BottomNavBar.vue
@@ -2,7 +2,7 @@
 import BottomNavigationItem from "@/Components/BottomNavigationItem.vue";
 import {computed, ref} from "vue";
 import {router, usePage} from "@inertiajs/vue3";
-import {LayoutDashboard, User, MapPin} from "lucide-vue-next";
+import {LayoutDashboard, User, MapPin, Gamepad, Wallet} from "lucide-vue-next";
 import Notification from "@/Components/Notification.vue";
 
 const user = computed(() => {
@@ -51,9 +51,14 @@ const toggleDropdown = (state) => {
             <slot />
         </main>
     <div class="fixed right-0 left-0 bottom-0 shadow-lg z-50">
-        <div class="flex w-full justify-around py-1.5 bg-white border-t border-gray-100 ">
+        <div v-if="user.type ==='agent'" class="flex w-full justify-around py-1.5 bg-white border-t border-gray-100 ">
             <BottomNavigationItem :icon="LayoutDashboard" label="Dashboard" to="/agent" :active="route().current('agent.home')" />
             <BottomNavigationItem v-if="user" :icon="MapPin" label="Branches" to="/agent/branches" :active="route().current('agent.branches')" />
+            <BottomNavigationItem :icon="User" label="Profile" to="/profile" :active="route().current('profile.edit')" />
+        </div>
+        <div v-if="user.type ==='cashier'" class="flex w-full justify-around py-1.5 bg-white border-t border-gray-100 ">
+            <BottomNavigationItem :icon="Gamepad" label="Play" to="/cashier/game/initiate" :active="route().current('cashier.game.initiate') || route().current('cashier.game.create')" />
+            <BottomNavigationItem :icon="Wallet" label="Wallet" to="/cashier/finance" :active="route().current('cashier.finance')" />
             <BottomNavigationItem :icon="User" label="Profile" to="/profile" :active="route().current('profile.edit')" />
         </div>
     </div>
