c35173ced890b88708dc8ffe38fce5824068c387
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-26 14:20:25 +0300

    Add coming soon page

---
+++ resources/js/Pages/ComingSoon.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {Construction} from "lucide-vue-next";
+</script>
+
+<template>
+    <div class="flex h-screen  justify-center">
+
+        <div class="flex flex-col space-y-4 h-3/5 items-center justify-center">
+            <Construction size="100" class="text-amber-400 fill-black"/>
+            <h1 class="text-2xl font-bold text-brand-100">Coming Soon</h1>
+            <p class="text-sm text-neutral-500">We are working on something awesome!</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
